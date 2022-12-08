9ddcf6032de712fb152782cb79f25d0c30395544
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2022-12-08 02:24:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {menuArray} from './data.js'
 
-
+let orderDiv = document.getElementById("ORDER-SUMMARY")
 let orderSummaryArray = []
 
 const orderItems = document.getElementById('order-items')
@@ -10,35 +10,54 @@ document.addEventListener('click', function(e){
     if (e.target.dataset.add){
         console.log(e.target.dataset.add)
         addToOrder(e.target.dataset.add)
+    } else if (e.target.dataset.remove){
+        console.log(e.target.dataset.remove)
+        RemoveFrmOrder(e.target.dataset.remove)
+    } else if (e.target.dataset.pay){
+        console.log(e.target.dataset.pay)
+        PayClick(e.target.dataset.pay)
     }
 })
+
+let TotalPrice =(menuId) =>{
+    return 'Price'
+}
+
+function RemoveFrmOrder(menuId){
+    orderSummaryArray.splice(menuId, 1);
+    render()
+}
+
 
 let addToOrder = (menuId) => {
     let itemObj = menuArray.filter((item)=>{
         return item.uuid === menuId
     })[0]
 
-    orderSummaryArray.push({
-        item: itemObj.item,
-        cost: itemObj.price
-    })
+    orderSummaryArray.push( itemObj )
 
-    console.log(orderSummaryArray.item)
-    
+    console.log(orderSummaryArray)
+    render()
 }
 
 function orderHtml(){
     let order = ""
     orderSummaryArray.forEach((item)=>{
         order += `
-        <h2 class="center"> Your order</h2>
-                            <div class="order-items">
-                            </div>
-                            <input type="button" placeholder="Complete Order" id="complete-btn"/>
-
+        
+        <div class="order-list">
+        <div class="inner-order-list">
+            <p><strong>${item.name}</strong></h2>
+            <button class="remove-btn" data-remove="${item.uuid}">remove</button>
+        </div>
+        <p><strong>$${item.price}</strong></h2>
+    </div> 
         `
     })
-}
+    return order
+    
+    } 
+
 
 function menuHtml(){
     let menu = ""
@@ -56,8 +75,8 @@ function menuHtml(){
                     <p><strong>$${item.price}</strong></p>
                 </div>
             </div>
-                <div class="item-wrapper">
-                <i class="fa-regular fa-plus plus"></i>
+                <div class="item-wrapper" >
+                <i class="fa-regular fa-plus plus" data-add="${item.uuid}"></i>
                 </div>        
         </div>
         `
@@ -66,7 +85,15 @@ function menuHtml(){
     return menu 
 }
 let render =()=>{
+    if (orderSummaryArray.length === 0){
+        document.getElementById('ORDER-SUMMARY').style.display ="none"
+    }
+    
 document.getElementById('MENU-ITEMS').innerHTML = menuHtml()
+document.getElementById("wrapper").innerHTML = orderHtml()
+document.getElementById("total-price").innerHTML = TotalPrice()
+
+
 
 }
 render()
